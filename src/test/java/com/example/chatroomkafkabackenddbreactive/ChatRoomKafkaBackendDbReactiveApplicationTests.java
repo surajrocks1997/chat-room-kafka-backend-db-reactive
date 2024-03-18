e91b354252cd9500a62fd178eb814556f223c5ec
Suraj Gupta <surajrocks1997@gmail.com>
2024-03-18 16:29:56 +0530

    Initial commit

---
+++ src/test/java/com/example/chatroomkafkabackenddbreactive/ChatRoomKafkaBackendDbReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatroomkafkabackenddbreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomKafkaBackendDbReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
