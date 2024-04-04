c4d95c7cd4388d3860cefc306338544314aa05a2
Suraj Gupta <surajrocks1997@gmail.com>
2024-04-04 23:11:19 +0530

    added DAO method to fetch only top 20 word count data

--- src/main/java/com/example/chatroomkafkabackenddbreactive/controller/InitController.java
+++ src/main/java/com/example/chatroomkafkabackenddbreactive/controller/InitController.java
@@ -5,6 +5,7 @@ import com.example.chatroomkafkabackenddbreactive.dao.WordCountDataRepository;
 import com.example.chatroomkafkabackenddbreactive.pojo.ChatRoomData;
 import com.example.chatroomkafkabackenddbreactive.pojo.WordCountData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -32,7 +33,7 @@ public class InitController {
 
     @GetMapping("/fetchWordData")
     public ResponseEntity<List<WordCountData>> getWordCountDataFromDB() {
-        List<WordCountData> allData = wordCountDataRepository.findAll();
-        return new ResponseEntity<>(allData, HttpStatus.OK);
+        List<WordCountData> topTwentyWordCountData = wordCountDataRepository.findTopWordCounts(PageRequest.of(0, 20));
+        return new ResponseEntity<>(topTwentyWordCountData, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/chatroomkafkabackenddbreactive/dao/WordCountDataRepository.java
+++ src/main/java/com/example/chatroomkafkabackenddbreactive/dao/WordCountDataRepository.java
@@ -1,7 +1,14 @@
 package com.example.chatroomkafkabackenddbreactive.dao;
 
 import com.example.chatroomkafkabackenddbreactive.pojo.WordCountData;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface WordCountDataRepository extends JpaRepository<WordCountData, String> {
+
+    @Query("SELECT w from WordCountData w ORDER BY w.count DESC")
+    List<WordCountData> findTopWordCounts(Pageable pageable);
 }
